6a8e6144b86fce4b0ba03492c853ed5ddd1c71e4
Jack Baron <jackmbaron@gmail.com>
2019-10-05 18:07:33 +0100

    ensure scenes are loaded upon game start

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(KeepObject))]
+public class SceneLoader : MonoBehaviour
+{
+    public List<SceneReference> _scenes;
+
+    void Start()
+    {
+        foreach (SceneReference _scene in _scenes) {
+            Scene scene = SceneManager.GetSceneByPath(_scene.ScenePath);
+            if (scene.isLoaded) continue;
+
+            SceneManager.LoadSceneAsync(_scene, LoadSceneMode.Additive);
+        }
+    }
+}
