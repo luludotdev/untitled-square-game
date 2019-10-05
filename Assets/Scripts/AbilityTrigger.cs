5c2dcaa61e429ee2a76eddcdb30b786b1a9a5d3d
Jack Baron <jackmbaron@gmail.com>
2019-10-05 02:33:29 +0100

    add script to trigger abilities

---
+++ Assets/Scripts/AbilityTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class AbilityTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerAbilities _abilities;
+
+    [SerializeField]
+    private Collider _collider;
+
+    [SerializeField]
+    private Ability _ability;
+
+    void Start()
+    {
+        if (_abilities == null) throw new NullReferenceException("Player Abilities cannot be null!");
+        if (_collider == null) throw new NullReferenceException("Collider cannot be null!");
+
+        _collider.isTrigger = true;
+    }
+
+    void OnTriggerEnter()
+    {
+        _abilities.Unlock(_ability);
+    }
+}
