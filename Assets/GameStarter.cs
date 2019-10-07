e7c2797c08e26c642e4b61ccde3baccaff856145
Jack Baron <jackmbaron@gmail.com>
2019-10-07 21:42:29 +0100

    start game

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    [SerializeField]
+    private SceneReference _mainScene;
+
+    public void StartGame() {
+        SceneManager.LoadSceneAsync(_mainScene);
+    }
+
+    public void QuitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
