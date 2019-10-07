d57d5674dd92510f8ab6908c479b76576ed9c258
Jack Baron <jackmbaron@gmail.com>
2019-10-07 19:56:33 +0100

    finish up end thing

--- Assets/TheEnd.cs
+++ Assets/TheEnd.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(SphereCollider))]
 public class TheEnd : MonoBehaviour
@@ -16,6 +17,9 @@ public class TheEnd : MonoBehaviour
     private float _floatOffset = 0.2f;
 
     private Vector3 _initialPosition;
+
+    public SceneReference _mainScene;
+    public SceneReference _endScene;
 
     void Start() {
         _collider = GetComponent<SphereCollider>();
@@ -34,6 +38,29 @@ public class TheEnd : MonoBehaviour
         Vector3 top = _initialPosition + vectorOffset;
         Vector3 bottom = _initialPosition - vectorOffset;
         transform.position = Vector3.Lerp(top, bottom, sin);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        StartCoroutine(LoadEndScene());
+        GlobalCamera.instance.SetZoom(6);
+    }
+
+    IEnumerator LoadEndScene() {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene.path == _mainScene.ScenePath) continue;
+            if (scene.path == _endScene.ScenePath) continue;
+
+            var task = SceneManager.UnloadSceneAsync(scene);
+            while (!task.isDone) yield return null;
+        }
+
+        var load = SceneManager.LoadSceneAsync(_endScene);
+        while (!load.isDone) yield return null;
     }
 
     void OnDrawGizmos()
