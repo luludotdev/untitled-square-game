22b6bf140487e17c4f0ea21275b1473fcf7de968
Jack Baron <jackmbaron@gmail.com>
2019-10-07 14:21:32 +0100

    only generate death meshes if a material is set

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -23,42 +23,44 @@ public class DeathTrigger : MonoBehaviour
         _collider = GetComponent<BoxCollider>();
         _collider.isTrigger = true;
 
-        Mesh mesh = new Mesh();
+        if (_material) {
+            Mesh mesh = new Mesh();
 
-        Vector3 center = Vector3.zero;
-        float x = _collider.bounds.size.x / 2;
-        float y = _collider.bounds.size.y / 2;
-        float z = (_collider.bounds.size.z / 2) - 0.1f;
+            Vector3 center = Vector3.zero;
+            float x = _collider.bounds.size.x / 2;
+            float y = _collider.bounds.size.y / 2;
+            float z = (_collider.bounds.size.z / 2) - 0.1f;
 
-        mesh.vertices = new Vector3[8] {
-            new Vector3(center.x + x, center.y + y, center.z + z),
-            new Vector3(center.x + x, center.y + y, center.z - z), // Top Right
-            new Vector3(center.x - x, center.y + y, center.z + z),
-            new Vector3(center.x - x, center.y + y, center.z - z), // Top Left
-            new Vector3(center.x + x, center.y - y, center.z + z),
-            new Vector3(center.x + x, center.y - y, center.z - z), // Bottom Right
-            new Vector3(center.x - x, center.y - y, center.z + z),
-            new Vector3(center.x - x, center.y - y, center.z - z), // Bottom Left
-        };
+            mesh.vertices = new Vector3[8] {
+                new Vector3(center.x + x, center.y + y, center.z + z),
+                new Vector3(center.x + x, center.y + y, center.z - z), // Top Right
+                new Vector3(center.x - x, center.y + y, center.z + z),
+                new Vector3(center.x - x, center.y + y, center.z - z), // Top Left
+                new Vector3(center.x + x, center.y - y, center.z + z),
+                new Vector3(center.x + x, center.y - y, center.z - z), // Bottom Right
+                new Vector3(center.x - x, center.y - y, center.z + z),
+                new Vector3(center.x - x, center.y - y, center.z - z), // Bottom Left
+            };
 
-        mesh.triangles = new int[] {
-            0,
-            1,
-            2,
-            2,
-            1,
-            3,
-            3,
-            1,
-            7,
-            1,
-            5,
-            7,
-        };
+            mesh.triangles = new int[] {
+                0,
+                1,
+                2,
+                2,
+                1,
+                3,
+                3,
+                1,
+                7,
+                1,
+                5,
+                7,
+            };
 
-        mesh.RecalculateNormals();
-        _filter.mesh = mesh;
-        _renderer.material = _material;
+            mesh.RecalculateNormals();
+            _filter.mesh = mesh;
+            _renderer.material = _material;
+        }
     }
 
     void OnTriggerEnter(Collider other)
