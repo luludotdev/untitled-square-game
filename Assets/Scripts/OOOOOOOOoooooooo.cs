003bafaf4b6ee042685d310503b6c208d29520b8
DerpyChap <holla@derpychap.co.uk>
2019-10-07 12:06:25 +0100

    Fade in OOOOOOOOOooooooooo

--- Assets/Scripts/OOOOOOOOoooooooo.cs
+++ Assets/Scripts/OOOOOOOOoooooooo.cs
@@ -12,6 +12,13 @@ public class OOOOOOOOoooooooo : MonoBehaviour
         _oooo = GetComponent<AudioSource>();
     }
 
+    
+    void FixedUpdate() {
+        if (_oooo.isPlaying && _oooo.volume < 1) {
+            _oooo.volume += Time.deltaTime / 5;
+        }
+    }
+
     public void OoooTime(Ability ability) {
         if (ability == Ability.Sound) {
             _oooo.Play();
